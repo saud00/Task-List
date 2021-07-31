f2182a65b7f2554069992add1c7419916e7454cb
Saud <gomail2017@gmail.com>
2021-07-31 23:50:22 +0500

    add task

--- App.js
+++ App.js
@@ -1,4 +1,30 @@
-const div1 = document.createElement("div");
-div1.id = 1;
-div1.className = "container";
-console.log(div1);
+const container = document.querySelector(".container");
+const inputTask = document.querySelector("#newInput");
+const filter = document.querySelector("#filter");
+
+//create Add Task Button
+const btn = document.createElement("button");
+btn.appendChild(document.createTextNode("Add Task"));
+btn.setAttribute("type", "submit");
+document.querySelector("#btn").appendChild(btn);
+
+const ul = document.createElement("ul");
+
+const ulDiv = document.querySelector("#ul");
+
+ulDiv.appendChild(ul);
+// input extract value from input
+let inputVal;
+
+inputTask.addEventListener("input", function (e) {
+  inputVal = e.target.value;
+});
+
+// Add Task to <ul>
+btn.addEventListener("click", function (e) {
+  const li = document.createElement("li");
+  li.className = "li";
+  const liChild = li.appendChild(document.createTextNode(inputVal));
+  ul.appendChild(li);
+  document.querySelector("#newInput").value = null;
+});
