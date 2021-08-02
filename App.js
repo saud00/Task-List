98fee579bdc715c0d104c7ca55514d934b7c69bc
Saud <gomail2017@gmail.com>
2021-08-03 00:04:19 +0500

    TaskList Project complete, added localStorage functionality

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container");
 const inputTask = document.querySelector("#newInput");
 const filter = document.querySelector("#filter");
+const LStoUI = document.addEventListener("DOMContentLoaded", showDataInUI);
 
 //create Add Task Button
 const btn = document.createElement("button");
@@ -30,7 +31,6 @@ btn.addEventListener("click", function (e) {
   li.className = "list-group-item text-capitalize";
   const liChild = li.appendChild(document.createTextNode(inputVal));
   ul.appendChild(li);
-  listArray.push(inputVal);
 
   link = document.createElement("a");
   link.innerHTML = `<i class="fas fa-times"></i>`;
@@ -38,13 +38,19 @@ btn.addEventListener("click", function (e) {
   li.appendChild(link);
   link.addEventListener("click", removeFunction);
 
+  saveDataToLS(inputVal);
   document.querySelector("#newInput").value = null;
 });
 
 // Remove perticular list item
+let removePerticular;
+
 function removeFunction(e) {
-  e.target.parentElement.parentElement.remove();
-  console.log("remove clicked");
+  removePerticular = e.target.parentElement.parentElement;
+  removePerticular.remove();
+
+  // remove from LS
+  removePerticularFromLS(removePerticular);
 }
 
 // Filter list
@@ -63,6 +69,8 @@ filter.addEventListener("input", function (e) {
   });
 });
 
+// Clear All tasks
+
 const clear = document.querySelector("#clear");
 clear.addEventListener("click", clearList);
 
@@ -70,5 +78,57 @@ function clearList() {
   const select = document.querySelectorAll(".list-group-item");
   select.forEach(function (e) {
     e.remove();
+    // clear form local storage
+    localStorage.clear();
+  });
+}
+
+// Saving to local storage
+
+function saveDataToLS(task) {
+  if (localStorage.getItem("listArray") === null) {
+    listArray = [];
+  } else {
+    listArray = JSON.parse(localStorage.getItem("listArray"));
+  }
+
+  listArray.push(task);
+
+  localStorage.setItem("listArray", JSON.stringify(listArray));
+}
+
+function showDataInUI(item) {
+  if (localStorage.getItem("listArray") === null) {
+    listArray = [];
+  } else {
+    listArray = JSON.parse(localStorage.getItem("listArray"));
+  }
+
+  listArray.forEach(function (val, ind) {
+    const li = document.createElement("li");
+    li.className = "list-group-item text-capitalize";
+    const liChild = li.appendChild(document.createTextNode(val));
+    ul.appendChild(li);
+
+    link = document.createElement("a");
+    link.innerHTML = `<i class="fas fa-times"></i>`;
+    link.className = "remove";
+    li.appendChild(link);
+    link.addEventListener("click", removeFunction);
+  });
+}
+
+function removePerticularFromLS(item) {
+  console.log(listArray, item);
+  listArray.forEach(function (val, ind) {
+    if (localStorage.getItem("listArray") === null) {
+      listArray = [];
+    } else {
+      listArray = JSON.parse(localStorage.getItem("listArray"));
+    }
+
+    if (item.textContent === val) listArray.splice(ind, 1);
+
+    localStorage.setItem("listArray", JSON.stringify(listArray));
   });
 }
